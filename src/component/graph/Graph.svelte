c27db3ae331baf107fd81385fedc1b8a1f1704bf
Collin D. Bevins <cbevins@montana.com>
2020-11-12 09:11:48 -0700

    Moved x- y- selectors onto table headers

--- src/component/graph/Graph.svelte
+++ src/component/graph/Graph.svelte
@@ -8,20 +8,9 @@
 
 <table>
   <tr>
-    <td>Graph X Variable</td>
-    <td><InputSelector bind:selected={$_input.graph.x.key} _input={_input}/></td>
-  </tr>
-  <tr>
-    <td>Graph Y Variable</td>
-    <td><OutputSelector bind:selected={$_input.graph.y.key} _output={_output}/></td>
-  </tr>
-</table>
-
-<table>
-  <tr>
     <td>Idx</td>
-    <td>{$_input.graph.x.key}</td>
-    <td>{$_input.graph.y.key}</td>
+    <td><InputSelector bind:selected={$_input.graph.x.key} _input={_input}/></td>
+    <td><OutputSelector bind:selected={$_input.graph.y.key} _output={_output}/></td>
   </tr>
   {#each $_input.graph.x.values.base as x, idx}
     <tr>
