810877a78a46a4c7e36471882057f10770fbd5c4
Collin D. Bevins <cbevins@montana.com>
2020-11-14 20:07:52 -0700

    collapsable input sliders

--- src/component/app/InputPanel.svelte
+++ src/component/app/InputPanel.svelte
@@ -15,48 +15,75 @@
   // export let _output
 </script>
 
-<div class='row'>
-  <div class='col'>
-    <FuelSelector bind:key={$_input.fuelModelCatalogKey.value.base}/>
-  </div>
-</div>
+<FuelSelector bind:key={$_input.fuelModelCatalogKey.value.base}/>
+<UnitsSelector bind:units={$_input.uomSlate} />
 
-<div class='row'>
-  <div class='col'>
-    <UnitsSelector bind:units={$_input.uomSlate} />
-  </div>
-</div>
-
-<div class='row'>
-  <div class='col-6'>
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#deadFuelMoisture" aria-controls="deadFuelMoisture">
+  Dead Fuel Moisture
+</button>
+<div class="collapse" id="deadFuelMoisture">
+  <div class="card card-body">
     <DeadFuelMoisture
       bind:tl1h={$_input.fuelMoistureDead1.value.base}
       bind:tl10h={$_input.fuelMoistureDead10.value.base}
       bind:tl100h={$_input.fuelMoistureDead100.value.base} />
   </div>
-  <div class='col'>
+</div>
+
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#liveFuelMoisture" aria-controls="liveFuelMoisture">
+  Live Fuel Moisture
+</button>
+<div class="collapse" id="liveFuelMoisture">
+  <div class="card card-body">
     <LiveFuelMoisture
       bind:herb={$_input.fuelMoistureLiveHerb.value.base}
       bind:stem={$_input.fuelMoistureLiveStem.value.base} />
   </div>
 </div>
 
-<div class='row'>
-  <div class='col-4'>
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#slopeAspect" aria-controls="slopeAspect">
+  Slope &amp; Aspect
+</button>
+<div class="collapse" id="slopeAspect">
+  <div class="card card-body">
     <SlopeAspect
       bind:steepness={$_input.slopeSteepnessRatio.value.base}
       bind:aspect={$_input.slopeDirectionAspect.value.base} />
   </div>
-  <div class='col-4'>
+</div>
+
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#windSpeedDirection" aria-controls="windSpeedDirection">
+  Wind Speed &amp; Direction
+</button>
+<div class="collapse" id="windSpeedDirection">
+  <div class="card card-body">
     <WindSpeedDirection
       bind:speed={$_input.windSpeedAtMidflame.value.base}
       bind:direction={$_input.windDirectionSourceFromNorth.value.base} />
   </div>
-  <div class='col-2'>
+</div>
+
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#airTemperature" aria-controls="airTemperature">
+  Air Temperature
+</button>
+<div class="collapse" id="airTemperature">
+  <div class="card card-body">
     <AirTemperature
       bind:f={$_input.airTemperature.value.base} />
   </div>
-  <div class='col-2'>
+</div>
+
+<button class="btn btn-outline-secondary btn-sm btn-block" data-toggle="collapse" type="button" aria-expanded="false"
+    data-target="#elapsedTime" aria-controls="elapsedTime">
+  Elapsed Time
+</button>
+<div class="collapse" id="elapsedTime">
+  <div class="card card-body">
     <ElapsedTime
       bind:minutes={$_input.timeSinceIgnition.value.base} />
   </div>
